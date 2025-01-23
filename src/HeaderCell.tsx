af1115b49de8ac80bb76dfa4aa7f9a3da8a5a92a
sarojregmi200 <sarojregmi.official@gmail.com>
2025-01-23 15:18:10 +0545

    test release

--- src/HeaderCell.tsx
+++ src/HeaderCell.tsx
@@ -121,22 +121,33 @@ const HeaderCell = React.forwardRef(
 
         const renderCustomizeIcon = () => {
             if (customizable && !groupHeader)
-                return (<span className='bt-h-5 bt-aspect-square bt-rounded-sm bt-flex bt-items-center bt-justify-center hover:bt-bg-gray-100 bt-cursor-pointer'
-                    onClick={(e) => {
-                        onHeaderCustomizeClick?.(props, e as React.MouseEvent)
-                    }} >
-                    <svg width="15" height="15" viewBox="0 0 21 21" fill="none" className='bt-rotate-90'>
-                        <path
-                            d="M5.66666 10.2529C5.66666 11.0029 5.05867 11.6109 4.30867 11.6109C3.55867 11.6109 2.95068 11.0029 2.95068 10.2529C2.95068 9.50295 3.55867 8.89496 4.30867 8.89496C5.05867 8.89496 5.66666 9.50295 5.66666 10.2529Z"
-                            fill="currentColor" />
-                        <path
-                            d="M18.7553 10.2529C18.7553 11.0029 18.1473 11.6109 17.3973 11.6109C16.6473 11.6109 16.0393 11.0029 16.0393 10.2529C16.0393 9.50295 16.6473 8.89496 17.3973 8.89496C18.1473 8.89496 18.7553 9.50295 18.7553 10.2529Z"
-                            fill="currentColor" />
-                        <path
-                            d="M12.2172 10.2529C12.2172 11.0029 11.6092 11.6109 10.8592 11.6109C10.1092 11.6109 9.50122 11.0029 9.50122 10.2529C9.50122 9.50295 10.1092 8.89496 10.8592 8.89496C11.6092 8.89496 12.2172 9.50295 12.2172 10.2529Z"
-                            fill="currentColor" />
-                    </svg>
-                </span>);
+                return (
+                    <span
+                        style={{
+                            height: '20px',
+                            width: '20px',
+                            borderRadius: '4px',
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            transform: 'rotate(90deg)',
+                        }}
+                        className='bt-header-cust-icon'
+                        onClick={(e) => {
+                            onHeaderCustomizeClick?.(props, e as React.MouseEvent)
+                        }} >
+                        <svg width="15" height="15" viewBox="0 0 21 21" fill="none" className='bt-rotate-90'>
+                            <path
+                                d="M5.66666 10.2529C5.66666 11.0029 5.05867 11.6109 4.30867 11.6109C3.55867 11.6109 2.95068 11.0029 2.95068 10.2529C2.95068 9.50295 3.55867 8.89496 4.30867 8.89496C5.05867 8.89496 5.66666 9.50295 5.66666 10.2529Z"
+                                fill="currentColor" />
+                            <path
+                                d="M18.7553 10.2529C18.7553 11.0029 18.1473 11.6109 17.3973 11.6109C16.6473 11.6109 16.0393 11.0029 16.0393 10.2529C16.0393 9.50295 16.6473 8.89496 17.3973 8.89496C18.1473 8.89496 18.7553 9.50295 18.7553 10.2529Z"
+                                fill="currentColor" />
+                            <path
+                                d="M12.2172 10.2529C12.2172 11.0029 11.6092 11.6109 10.8592 11.6109C10.1092 11.6109 9.50122 11.0029 9.50122 10.2529C9.50122 9.50295 10.1092 8.89496 10.8592 8.89496C11.6092 8.89496 12.2172 9.50295 12.2172 10.2529Z"
+                                fill="currentColor" />
+                        </svg>
+                    </span>);
             return null;
         };
 
@@ -155,12 +166,18 @@ const HeaderCell = React.forwardRef(
                         align={!groupHeader ? align : undefined}
                         verticalAlign={!groupHeader ? verticalAlign : undefined}
                     >
-                        {/* // TODO: tailwind remove */}
-                        <div className="bt-wrapper bt-flex bt-items-center bt-gap-2.5">
+                        <div
+                            style={{
+                                height: '100%',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                gap: '4px',
+                            }}>
                             {children}
                             {renderCustomizeIcon()}
                         </div>
-                    </Cell>
+                    </Cell >
                 </div >
             );
 
