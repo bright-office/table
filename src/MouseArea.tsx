100f0cb02a46f7f2e60d60fb9641a37bc2617409
Saroj <sarojregmi.official@gmail.com>
2025-05-02 20:58:45 +0545

    feat: Table Height with auto height is now fixed and working fine.

--- src/MouseArea.tsx
+++ src/MouseArea.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 
 interface MouseAreaProps extends React.HTMLAttributes<HTMLDivElement> {
-  addPrefix: (...classes: any) => string;
-  height: number;
-  headerHeight: number;
+    addPrefix: (...classes: any) => string;
+    height: number;
+    headerHeight: number;
 }
 
 const MouseArea = React.forwardRef((props: MouseAreaProps, ref: React.Ref<HTMLDivElement>) => {
-  const { addPrefix, headerHeight, height } = props;
-  const styles = { height };
+    const { addPrefix, headerHeight, height } = props;
+    const styles = { height };
 
-  const spanStyles = { height: headerHeight - 1 };
-  return (
-    <div ref={ref} className={addPrefix('mouse-area')} style={styles}>
-      <span style={spanStyles} />
-    </div>
-  );
+    const spanStyles = { height: headerHeight - 1 };
+    return (
+        <div ref={ref} className={addPrefix('mouse-area')} style={styles} data-mouse="true">
+            <span style={spanStyles} />
+        </div>
+    );
 });
 
 MouseArea.displayName = 'Table.MouseArea';
